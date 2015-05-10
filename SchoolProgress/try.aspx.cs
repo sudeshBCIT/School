7eb6744b4c49a7ef7bc2371d34af5d5048aa5456
sudesh_9070 <sudesh_kumar88@yahoo.com>
2015-05-09 18:24:35 -0700

    h
    v

--- SchoolProgress/try.aspx.cs
+++ SchoolProgress/try.aspx.cs
@@ -13,5 +13,10 @@ namespace SchoolProgress
         {
 
         }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("subject.aspx");
+        }
     }
 }
